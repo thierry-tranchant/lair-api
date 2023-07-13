df95f5bb9dca8e94e3aea27febea28524d621e36
Thierry <thierry@macbook-pro-de-thierry.home>
2023-07-13 08:58:17 +0200

    Fix client

--- lib/OneSignalClient.rb
+++ lib/OneSignalClient.rb
@@ -1,23 +1,25 @@
 class OneSignalClient
   URL = "https://onesignal.com/api/v1"
 
-  def call(user, content)
-    url = URI("#{URL}/notifications")
+  class << self
+    def call(user, content)
+      url = URI("#{URL}/notifications")
 
-    http = Net::HTTP.new(url.host, url.port)
-    http.use_ssl = true
+      http = Net::HTTP.new(url.host, url.port)
+      http.use_ssl = true
 
-    request = Net::HTTP::Post.new(url)
-    request["Accept"] = 'application/json'
-    request["Authorization"] = "Basic #{Rails.application.credentials.dig(:one_signal, :api_key)}"
-    request["Content-Type"] = 'application/json'
+      request = Net::HTTP::Post.new(url)
+      request["Accept"] = 'application/json'
+      request["Authorization"] = "Basic #{Rails.application.credentials.dig(:one_signal, :api_key)}"
+      request["Content-Type"] = 'application/json'
 
-    request.body = {
-      app_id: Rails.application.credentials.dig(:one_signal, :app_id),
-      include_player_ids: [user.one_signal_player_id],
-      contents: {fr: content, en: "You have received a message"}
-    }.to_json
+      request.body = {
+        app_id: Rails.application.credentials.dig(:one_signal, :app_id),
+        include_player_ids: [user.one_signal_player_id],
+        contents: {fr: content, en: "You have received a message"}
+      }.to_json
 
-    http.request(request)
+      http.request(request)
+    end
   end
 end
